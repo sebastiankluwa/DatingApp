d7def09265b0ee18d56dfb5ba1c3a728d619f187
Sebastian Kluwa <s.kluwa@i4b.pl>
2021-12-15 08:27:06 +0100

    Design of menu

--- DatingApp/Form1.Designer.cs
+++ DatingApp/Form1.Designer.cs
@@ -29,13 +29,200 @@ namespace DatingApp
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.btnMessages = new FontAwesome.Sharp.IconButton();
+            this.btnLists = new FontAwesome.Sharp.IconButton();
+            this.btnMatches = new FontAwesome.Sharp.IconButton();
+            this.btnHome = new FontAwesome.Sharp.IconButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelTitleBar = new System.Windows.Forms.Panel();
+            this.panelMenu.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(1167, 11);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(21, 18);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnHome_Click);
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(30)))), ((int)(((byte)(68)))));
+            this.panelMenu.Controls.Add(this.btnMessages);
+            this.panelMenu.Controls.Add(this.btnLists);
+            this.panelMenu.Controls.Add(this.btnMatches);
+            this.panelMenu.Controls.Add(this.btnHome);
+            this.panelMenu.Controls.Add(this.panel2);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(220, 703);
+            this.panelMenu.TabIndex = 1;
+            // 
+            // btnMessages
+            // 
+            this.btnMessages.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMessages.FlatAppearance.BorderSize = 0;
+            this.btnMessages.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMessages.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnMessages.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnMessages.IconChar = FontAwesome.Sharp.IconChar.Compass;
+            this.btnMessages.IconColor = System.Drawing.Color.White;
+            this.btnMessages.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnMessages.IconSize = 32;
+            this.btnMessages.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMessages.Location = new System.Drawing.Point(0, 320);
+            this.btnMessages.Name = "btnMessages";
+            this.btnMessages.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnMessages.Size = new System.Drawing.Size(220, 60);
+            this.btnMessages.TabIndex = 3;
+            this.btnMessages.Text = "Messages";
+            this.btnMessages.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMessages.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnMessages.UseVisualStyleBackColor = true;
+            this.btnMessages.Click += new System.EventHandler(this.btnMessages_Click);
+            // 
+            // btnLists
+            // 
+            this.btnLists.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnLists.FlatAppearance.BorderSize = 0;
+            this.btnLists.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLists.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnLists.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnLists.IconChar = FontAwesome.Sharp.IconChar.Compass;
+            this.btnLists.IconColor = System.Drawing.Color.White;
+            this.btnLists.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnLists.IconSize = 32;
+            this.btnLists.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLists.Location = new System.Drawing.Point(0, 260);
+            this.btnLists.Name = "btnLists";
+            this.btnLists.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnLists.Size = new System.Drawing.Size(220, 60);
+            this.btnLists.TabIndex = 2;
+            this.btnLists.Text = "Lists";
+            this.btnLists.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLists.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnLists.UseVisualStyleBackColor = true;
+            this.btnLists.Click += new System.EventHandler(this.btnLists_Click);
+            // 
+            // btnMatches
+            // 
+            this.btnMatches.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMatches.FlatAppearance.BorderSize = 0;
+            this.btnMatches.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMatches.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnMatches.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnMatches.IconChar = FontAwesome.Sharp.IconChar.Compass;
+            this.btnMatches.IconColor = System.Drawing.Color.White;
+            this.btnMatches.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnMatches.IconSize = 32;
+            this.btnMatches.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMatches.Location = new System.Drawing.Point(0, 200);
+            this.btnMatches.Name = "btnMatches";
+            this.btnMatches.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnMatches.Size = new System.Drawing.Size(220, 60);
+            this.btnMatches.TabIndex = 1;
+            this.btnMatches.Text = "Matches";
+            this.btnMatches.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMatches.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnMatches.UseVisualStyleBackColor = true;
+            this.btnMatches.Click += new System.EventHandler(this.btnMatches_Click);
+            // 
+            // btnHome
+            // 
+            this.btnHome.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnHome.FlatAppearance.BorderSize = 0;
+            this.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHome.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnHome.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnHome.IconChar = FontAwesome.Sharp.IconChar.Compass;
+            this.btnHome.IconColor = System.Drawing.Color.White;
+            this.btnHome.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnHome.IconSize = 32;
+            this.btnHome.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnHome.Location = new System.Drawing.Point(0, 140);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnHome.Size = new System.Drawing.Size(220, 60);
+            this.btnHome.TabIndex = 0;
+            this.btnHome.Text = "Home";
+            this.btnHome.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnHome.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnHome.UseVisualStyleBackColor = true;
+            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(220, 140);
+            this.panel2.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DatingApp.App.Properties.Resources.Tinder_743x420;
+            this.pictureBox1.Location = new System.Drawing.Point(-3, -3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(226, 131);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panelTitleBar
+            // 
+            this.panelTitleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(25)))), ((int)(((byte)(62)))));
+            this.panelTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitleBar.Location = new System.Drawing.Point(220, 0);
+            this.panelTitleBar.Name = "panelTitleBar";
+            this.panelTitleBar.Size = new System.Drawing.Size(979, 75);
+            this.panelTitleBar.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1199, 703);
+            this.Controls.Add(this.panelTitleBar);
+            this.Controls.Add(this.panelMenu);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelMenu.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panelMenu;
+        private FontAwesome.Sharp.IconButton btnHome;
+        private System.Windows.Forms.Panel panel2;
+        private FontAwesome.Sharp.IconButton btnMessages;
+        private FontAwesome.Sharp.IconButton btnLists;
+        private FontAwesome.Sharp.IconButton btnMatches;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panelTitleBar;
     }
 }
 
--- DatingApp/Form1.cs
+++ DatingApp/Form1.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using FontAwesome.Sharp;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,10 +14,130 @@ namespace DatingApp
 {
     public partial class Form1 : Form
     {
+        //Fields
+        private IconButton currentBtn;
+        private Panel leftBorderBtn;
+        private Form currentChildForm;
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,
+            int nTopRect,
+            int nRightRect,
+            int nBottomRect,
+            int nWidthEllipse,
+            int nHeightEllipse
+        );
+
         public Form1()
         {
             InitializeComponent();
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 25, 25));
+            leftBorderBtn = new Panel();
+            leftBorderBtn.Size = new Size(7, 60);
+            panelMenu.Controls.Add(leftBorderBtn);
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //Structs
+        private struct RGBColors
+        {
+            public static Color color1 = Color.FromArgb(172, 126, 241);
+            public static Color color2 = Color.FromArgb(249, 118, 176);
+            public static Color color3 = Color.FromArgb(253, 138, 114);
+            public static Color color4 = Color.FromArgb(95, 77, 221);
+            public static Color color5 = Color.FromArgb(249, 88, 155);
+            public static Color color6 = Color.FromArgb(24, 161, 251);
+        }
+
+        private void ActivateButton(object senderBtn, Color color)
+        {
+            if(senderBtn != null)
+            {
+                DisableButton();
+                //Button;
+                currentBtn = (IconButton)senderBtn;
+                currentBtn.BackColor = Color.FromArgb(37, 36, 81);
+                currentBtn.ForeColor = color;
+                currentBtn.TextAlign = ContentAlignment.MiddleCenter;
+                currentBtn.IconColor = color;
+                currentBtn.TextImageRelation = TextImageRelation.TextBeforeImage;
+                currentBtn.ImageAlign = ContentAlignment.MiddleRight;
+                //Left border button
+                leftBorderBtn.BackColor = color;
+                leftBorderBtn.Location = new Point(0, currentBtn.Location.Y);
+                leftBorderBtn.Visible = true;
+                leftBorderBtn.BringToFront();
+            }
+        }
+
+        private void DisableButton()
+        {
+            if(currentBtn != null)
+            {
+                currentBtn.BackColor = Color.FromArgb(31, 30, 68);
+                currentBtn.ForeColor = Color.Gainsboro;
+                currentBtn.TextAlign = ContentAlignment.MiddleLeft;
+                currentBtn.IconColor = Color.Gainsboro;
+                currentBtn.TextImageRelation = TextImageRelation.ImageBeforeText;
+                currentBtn.ImageAlign = ContentAlignment.MiddleLeft;
+            }
+        }
+        
+        private void OpenChildForm(Form childForm)
+        {
+            //open only form
+            if (currentChildForm != null)
+            {
+                currentChildForm.Close();
+            }
+            currentChildForm = childForm;
+            //End
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            panelDesktop.Controls.Add(childForm);
+            panelDesktop.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+            lblTitleChildForm.Text = childForm.Text;
+        }
+        private void Reset()
+        {
+            DisableButton();
+            leftBorderBtn.Visible = false;
+            iconCurrentChildForm.IconChar = IconChar.Home;
+            iconCurrentChildForm.IconColor = Color.MediumPurple;
+            lblTitleChildForm.Text = "Home";
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color1);
+            //OpenChildForm(new FormDashboard());
+        }
+
+        private void btnMatches_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color2);
+        }
+
+        private void btnLists_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color3);
+
+        }
+
+        private void btnMessages_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color4);
+
+        }
     }
 }
